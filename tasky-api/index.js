4bec5294d0caaace217bf1b5ba5401d681054729
Dawid <20098868@mail.wit.ie>
2023-12-10 16:13:27 +0100

    Mongo lab Error Handling

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -5,12 +5,21 @@ import './db';
 
 dotenv.config();
 
+const errHandler = (err, req, res, next) => {
+    if(process.env.NODE_ENV === 'production') {
+        return res.status(500).send(`Something went wrong!`);
+    }
+    res.status(500).send(`Hey!! You caught the error . Here's the details: ${err.stack}`);
+};
+
 const app = express();
 const port = process.env.port;
 
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+
+app.use(errHandler);
 
 app.listen(port, () => {
     console.info(`Server running at ${port}`);
